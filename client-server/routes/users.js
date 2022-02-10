0f23b51c4daec61a2e6da38e41d58353d49ed958
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-10 10:24:26 -0300

    Recriando a pasta

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,7 +1,12 @@
 var express = require('express');
-var restify = require('restify-clients')
+var restify = require('restify-clients') //Dando require no framework restfy
 var router = express.Router();
 
+var client = restify.createJSONClient({ //declaração de onde se encontra o serviço do nosso servidor
+  //client. no caso rodando no localhost 4000
+URL: 'https://localhost:4000'
+
+})
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
