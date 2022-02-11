b158f772ef9ba9f43365867c790ed71c3d840c6e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-10 21:58:15 -0300

    Chamando os objetos do servidor do cliente e retornando os usuários do outro servidor

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -5,14 +5,14 @@ var router = express.Router();
 
 var client = restify.createJSONClient({ //declaração de onde se encontra o serviço do nosso servidor
   //client. no caso rodando no localhost 4000
-URL: 'https://localhost:4000'
+url: 'http://localhost:4000'
 
 })
 /* GET users listing. */
 router.get('/', function(req, res, next) {//ja temos uma rota pra / que ta dentro do arquivo users.js
   client.get('/users', function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
-    assert.iferror(err);
-    console.log(JSON.stringify(obj, null, 2)) 
+    assert.ifError(err);
+    res.end(JSON.stringify(obj, null, 2)) //A resposta vai ser o objeto JSON caso de erro
     //toda vez que chamaram / vao chamar também a barra users
   });
 });
