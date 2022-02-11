a1d1f922794bf2e39d90682f1219e52e9e27da78
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-10 21:26:27 -0300

    documentando a chamada do client e dando require no assert

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var restify = require('restify-clients') //Dando require no framework restfy
+var assert = require('assert') //Dando require no assert
 var router = express.Router();
 
 var client = restify.createJSONClient({ //declaração de onde se encontra o serviço do nosso servidor
@@ -8,10 +9,11 @@ URL: 'https://localhost:4000'
 
 })
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  client.get('/users', function(err, req, res, obj) {
+router.get('/', function(req, res, next) {//ja temos uma rota pra / que ta dentro do arquivo users.js
+  client.get('/users', function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
     assert.iferror(err);
-    console.log(JSON.stringify(obj, null, 2))
+    console.log(JSON.stringify(obj, null, 2)) 
+    //toda vez que chamaram / vao chamar também a barra users
   });
 });
 
