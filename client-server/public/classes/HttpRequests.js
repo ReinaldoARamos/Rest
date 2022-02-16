7e39d1b1204d81a765d5dc6b71f0007de79b0304
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-15 22:22:07 -0300

    podemos usar esse método para diversas coisas, entao nela passamos o método(get, push, put etc), url(a rota) e os parametros caso tenham, caso nao apenas retorna um objeto vazio

--- client-server/public/classes/HttpRequests.js
+++ client-server/public/classes/HttpRequests.js
@@ -1,17 +1,21 @@
 class HttpRequest {
 
-    static request() { //colocamos um método estático porque desse jeito podemos chamar o método 
+    static request(method/*get set etc*/, url/* a rota* */, params = {}) { 
+        //colocamos um método estático porque desse jeito podemos chamar o método 
         //fora da classe de forma direta, sme precisar criar uma intância da classe
-        let ajax = new XMLHttpRequest; //criando a solicitação https XML
+        let ajax = new XMLHttpRequest; 
+        //criando a solicitação https XML
 
-        ajax.open("GET", "/users"); //Aqui eu tenho que definir o método que o ajax vai usar e onde usar
+        ajax.open("GET", "/users"); 
+        //Aqui eu tenho que definir o método que o ajax vai usar e onde usar
         ajax.onload = (event) => {
           // aqui criamos um evento que executa quando o request é carregado
     
           let obj = { users: [] }; //criado um obj como array users vazio pra nao ter problema no foreach
     
           try {
-            obj = JSON.parse(ajax.responseText); //executa um try catch, se retornar um JSON válido ele subscreve...caso nao ele da erro
+            obj = JSON.parse(ajax.responseText); 
+            //executa um try catch, se retornar um JSON válido ele subscreve...caso nao ele da erro
             //Esse evento aqui cria um let obj que contém a informação que o servidor retornou
             //como é um JSON é feito um parse
             //Como faremos um parse do JSON, ele vai pegar a informação e tranformar em um nod
