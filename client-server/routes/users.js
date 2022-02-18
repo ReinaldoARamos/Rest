f30089197bfd1be986ff082ea254827290822221
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-17 22:25:34 -0300

    Aqui criei o método put

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -37,7 +37,7 @@ router.get('/:id', function(req, res, next) {//ja temos uma rota pra / que ta de
   
 });
 router.put('/:id', function(req, res, next) {//ja temos uma rota pra / que ta dentro do arquivo users.js
-  client.put(`/users/${req.params.id}`,req.body ,function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
+  client.put(`/users/${req.params.id}`,req.body/*reebe o body com as informaçoes do json*/  ,function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
     //que busca as informações dentro da mesma, noc aso os users do rest-api
     assert.ifError(err);
     res.json(obj) 
