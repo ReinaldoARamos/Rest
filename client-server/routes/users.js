a85501cbcca4a626ae7dc36c76ca67d748c15522
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-02-17 22:18:48 -0300

    documentada

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -14,8 +14,10 @@ router.get('/', function(req, res, next) {//ja temos uma rota pra / que ta dentr
   client.get('/users', function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
     //que busca as informações dentro da mesma, noc aso os users do rest-api
     assert.ifError(err);
-    res.json(obj) //A resposta vai ser o objeto JSON caso de erro
-    //toda vez que chamaram / vao chamar também a barra users
+    res.json(obj)  
+    //passamos assim pra pra ao invés de usar o JSON Stringfy ele ja interptera
+    //o JSOn, no caso o obj
+    //------
   });
 
   
@@ -24,7 +26,10 @@ router.get('/:id', function(req, res, next) {//ja temos uma rota pra / que ta de
   client.get(`/users/${req.params.id}`, function(err, request, response, obj) { //criamos aqui a chamada do client na rota users
     //que busca as informações dentro da mesma, noc aso os users do rest-api
     assert.ifError(err);
-    res.json(obj) //A resposta vai ser o objeto JSON caso de erro
+    res.json(obj) 
+    //passamos assim pra pra ao invés de usar o JSON Stringfy ele ja interptera
+    //o JSOn, no caso o obj
+    //------
     //toda vez que chamaram / vao chamar também a barra users
   });
 
